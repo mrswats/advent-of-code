91b5af561b7b0d62ad8cb4d6896a6f3f6192abf3
Ferran Jovell <ferran.jovell@hybridtheory.com>
2022-11-16 08:40:53 +0100

    More WIP

--- src/day_4/main.py
+++ src/day_4/main.py
@@ -1,15 +1,19 @@
 import re
+from collections import OrderedDict
 
 COLS = 5
 ROWS = 5
+
+FILENAME = "input.txt"
 
 
 def parse_file(raw_input: list[str]) -> tuple[tuple[int], tuple[int]]:
     bingo_numbers = [int(num) for num in raw_input[0].split(",")]
     boards = tuple(
         tuple(
-            {int(num): False for num in re.findall(r"(\d+)", row)}
+            OrderedDict((int(num), False) for num in re.findall(r"(\d+)", row))
             for row in board.split("\n")
+            if row
         )
         for board in raw_input[1:]
     )
@@ -17,20 +21,30 @@ def parse_file(raw_input: list[str]) -> tuple[tuple[int], tuple[int]]:
     return bingo_numbers, boards
 
 
-def check_rows():
-    pass
+def check_rows(board) -> bool:
+    for row in board:
+        if row and all(value for value in row.values()):
+            return True
+    else:
+        return False
 
 
-def check_cols():
-    pass
+def check_cols(board) -> bool:
+    for ith in range(COLS):
+        if all(row[list(row.keys())[ith]] for row in board):
+            return True
+    else:
+        False
 
 
-def calculate_scores(boards):
-    pass
+def calculate_score(board, drawn_number):
+    return drawn_number * sum(
+        key for row in board for key, value in row.items() if not value
+    )
 
 
 def main() -> int:
-    with open("./input.txt") as fo:
+    with open(FILENAME) as fo:
         raw_input = fo.read().split("\n\n")
 
     bingo_numbers, boards = parse_file(raw_input)
@@ -41,11 +55,10 @@ def main() -> int:
                 if drawn_number in row:
                     row[drawn_number] = True
 
-        if check_rows() or check_cols():
+        if check_cols(board) or check_rows(board):
             print("BINGOOO!!!!!")
+            print(f"{calculate_score(board, drawn_number) = }")
             break
-
-    calculate_scores(boards)
 
     return 0
 
