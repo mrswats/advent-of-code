cdeac88149f7d2aa2f904a1e5d3f72a7692d9f5e
Ferran Jovell <ferran@jovell.dev>
2022-11-20 21:19:25 +0100

    Day 7 part 1 DONE

--- src/day_7/main.py
+++ src/day_7/main.py
@@ -13,9 +13,21 @@ def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument("filename")
     args = parser.parse_args(argv)
-    parsed_input = parse_input(args.filename)
+    crab_positions = parse_input(args.filename)
 
-    print(parsed_input)
+    max_coord = max(crab_positions)
+
+    min_fuel = 10000000000000000000
+
+    for position in range(max_coord):
+        fuel = 0
+        for crab in crab_positions:
+            fuel += abs(crab - position)
+
+        if fuel < min_fuel:
+            min_fuel = fuel
+
+    print(f"The calculated minimm amount of fuel is {min_fuel}")
 
     return 0
 
