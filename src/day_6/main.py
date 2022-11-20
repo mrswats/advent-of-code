0c88109792f5a375a21b741ec70c5415e0b1c98c
Ferran Jovell <ferran@jovell.dev>
2022-11-20 15:14:38 +0100

    Day 5: Part 2 DONE!

--- src/day_6/main.py
+++ src/day_6/main.py
@@ -1,26 +1,35 @@
 import argparse
+from collections import Counter
 from typing import Sequence
 
-TOTAL_DAYS = 80
+TOTAL_DAYS = 256
 
 
-def total_fish(population: list[int]) -> int:
-    return len(population)
+def total_fish(population: Counter[int, int]) -> int:
+    return population.total()
 
 
-def pass_a_day(population: list[int]) -> list[int]:
-    new_population = [fish - 1 for fish in population]
-    new_fish = new_population.count(-1)
-    new_population = [fish if fish >= 0 else 6 for fish in new_population]
+def pass_a_day(population: Counter[int, int]) -> Counter[int, int]:
+    new_population = {
+        0: population[1],
+        1: population[2],
+        2: population[3],
+        3: population[4],
+        4: population[5],
+        5: population[6],
+        6: population[7] + population[0],
+        7: population[8],
+        8: population[0],
+    }
 
-    return new_population + [8] * new_fish
+    return Counter(new_population)
 
 
-def parse_input(filename: str) -> list[int]:
+def parse_input(filename: str) -> Counter[int, int]:
     with open(filename) as f:
         filecontents = f.read()
 
-    return list(map(int, filecontents.split(",")))
+    return Counter(map(int, filecontents.split(",")))
 
 
 def main(argv: Sequence[str] | None = None) -> int:
@@ -29,10 +38,10 @@ def main(argv: Sequence[str] | None = None) -> int:
     args = parser.parse_args(argv)
     population = parse_input(args.filename)
 
-    for _ in range(TOTAL_DAYS):
+    for days_passed in range(TOTAL_DAYS):
         population = pass_a_day(population)
 
-    print(f"Total fish after {TOTAL_DAYS} days: {total_fish(population)}")
+    print(f"Total fish after {days_passed+1} days: {total_fish(population)}")
 
     return 0
 
