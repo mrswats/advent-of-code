506d256ca4ac096b88641c9018a4113e3e66035c
Ferran Jovell <ferran@jovell.dev>
2022-11-29 23:00:17 +0100

    Day 9 WIP

--- src/day_9/main.py
+++ src/day_9/main.py
@@ -1,5 +1,6 @@
 import argparse
-from typing import Sequence
+from dataclasses import dataclass
+from typing import Sequence, Set
 
 WIDTH = 100
 HEIGHT = 100
@@ -23,90 +24,102 @@ def parse_input(filename: str) -> list[int]:
     return [int(num) for num in filecontents.replace("\n", "")]
 
 
-def is_minima(height: int, *adjacent_edges: int) -> bool:
-    return all(height < adjacent for adjacent in adjacent_edges)
-
-
 def index(row: int, col: int) -> int:
     return col + row * WIDTH
 
 
-def find_minimal_heights(heatmap: list[(int, int)]):
-    minimal_heights = []
-    for row_idx in range(HEIGHT):
-        for col_idx in range(WIDTH):
-            current_index = index(row_idx, col_idx)
-            height = heatmap[current_index]
-            if row_idx == TOP_EDGE and col_idx == LEFT_EDGE:
-                if is_minima(
-                    height,
-                    heatmap[index(row_idx, col_idx + 1)],
-                    heatmap[index(row_idx + 1, col_idx)],
-                ):
-                    minimal_heights.append((current_index, height + 1))
-            elif row_idx == TOP_EDGE and col_idx == RIGHT_EDGE:
-                if is_minima(
-                    height,
-                    heatmap[index(row_idx, col_idx - 1)],
-                    heatmap[index(row_idx + 1, col_idx)],
-                ):
-                    minimal_heights.append((current_index, height + 1))
-            elif row_idx == BOTTOM_EDGE and col_idx == LEFT_EDGE:
-                if is_minima(
-                    height,
-                    heatmap[index(row_idx, col_idx + 1)],
-                    heatmap[index(row_idx - 1, col_idx)],
-                ):
-                    minimal_heights.append((current_index, height + 1))
-            elif row_idx == BOTTOM_EDGE and col_idx == RIGHT_EDGE:
-                if is_minima(
-                    height,
-                    heatmap[index(row_idx, col_idx - 1)],
-                    heatmap[index(row_idx - 1, col_idx)],
-                ):
-                    minimal_heights.append((current_index, height + 1))
-            elif col_idx == LEFT_EDGE:
-                if is_minima(
-                    height,
-                    heatmap[index(row_idx, col_idx + 1)],
-                    heatmap[index(row_idx + 1, col_idx)],
-                    heatmap[index(row_idx - 1, col_idx)],
-                ):
-                    minimal_heights.append((current_index, height + 1))
-            elif col_idx == RIGHT_EDGE:
-                if is_minima(
-                    height,
-                    heatmap[index(row_idx, col_idx - 1)],
-                    heatmap[index(row_idx + 1, col_idx)],
-                    heatmap[index(row_idx - 1, col_idx)],
-                ):
-                    minimal_heights.append((current_index, height + 1))
-            elif row_idx == TOP_EDGE:
-                if is_minima(
-                    height,
-                    heatmap[index(row_idx, col_idx - 1)],
-                    heatmap[index(row_idx, col_idx + 1)],
-                    heatmap[index(row_idx + 1, col_idx)],
-                ):
-                    minimal_heights.append((current_index, height + 1))
-            elif row_idx == BOTTOM_EDGE:
-                if is_minima(
-                    height,
-                    heatmap[index(row_idx, col_idx - 1)],
-                    heatmap[index(row_idx, col_idx + 1)],
-                    heatmap[index(row_idx - 1, col_idx)],
-                ):
-                    minimal_heights.append((current_index, height + 1))
-            else:
-                if is_minima(
-                    height,
-                    heatmap[index(row_idx, col_idx - 1)],
-                    heatmap[index(row_idx, col_idx + 1)],
-                    heatmap[index(row_idx - 1, col_idx)],
-                    heatmap[index(row_idx + 1, col_idx)],
-                ):
-                    minimal_heights.append((current_index, height + 1))
-    return minimal_heights
+def neighbours(row_idx: int, col_idx: int) -> Set[int]:
+    if row_idx == TOP_EDGE and col_idx == LEFT_EDGE:
+        return {
+            index(row_idx, col_idx + 1),
+            index(row_idx + 1, col_idx),
+        }
+    elif row_idx == TOP_EDGE and col_idx == RIGHT_EDGE:
+        return {
+            index(row_idx, col_idx - 1),
+            index(row_idx + 1, col_idx),
+        }
+    elif row_idx == BOTTOM_EDGE and col_idx == LEFT_EDGE:
+        return {
+            index(row_idx, col_idx + 1),
+            index(row_idx - 1, col_idx),
+        }
+    elif row_idx == BOTTOM_EDGE and col_idx == RIGHT_EDGE:
+        return {
+            index(row_idx, col_idx - 1),
+            index(row_idx - 1, col_idx),
+        }
+    elif col_idx == LEFT_EDGE:
+        return {
+            index(row_idx, col_idx + 1),
+            index(row_idx + 1, col_idx),
+            index(row_idx - 1, col_idx),
+        }
+    elif col_idx == RIGHT_EDGE:
+        return {
+            index(row_idx, col_idx - 1),
+            index(row_idx + 1, col_idx),
+            index(row_idx - 1, col_idx),
+        }
+    elif row_idx == TOP_EDGE:
+        return {
+            index(row_idx, col_idx - 1),
+            index(row_idx, col_idx + 1),
+            index(row_idx + 1, col_idx),
+        }
+    elif row_idx == BOTTOM_EDGE:
+        return {
+            index(row_idx, col_idx - 1),
+            index(row_idx, col_idx + 1),
+            index(row_idx - 1, col_idx),
+        }
+    else:
+        return {
+            index(row_idx, col_idx - 1),
+            index(row_idx, col_idx + 1),
+            index(row_idx - 1, col_idx),
+            index(row_idx + 1, col_idx),
+        }
+
+
+def is_minima(height: int, *height_adjacent_edges: int) -> bool:
+    return all(height < adjacent for adjacent in height_adjacent_edges)
+
+
+def find_minimal_heights(heatmap: list[int]) -> list[int]:
+    return [
+        index(row_idx, col_idx)
+        for row_idx in range(HEIGHT)
+        for col_idx in range(WIDTH)
+        if is_minima(
+            heatmap[index(row_idx, col_idx)],
+            *(heatmap[idx] for idx in neighbours(row_idx, col_idx)),
+        )
+    ]
+
+
+def search(heatmap: list[int]):
+    starting_nodes = find_minimal_heights(heatmap)
+    all_nodes = [
+        (row_idx, col_idx)
+        for row_idx in range(HEIGHT)
+        for col_idx in range(WIDTH)
+        if index(row_idx, col_idx) not in starting_nodes
+    ]
+
+    visited_nodes = []
+
+    current_node = starting_nodes[0]
+
+    while True:
+        visited_nodes.append(current_node)
+        neighs = [
+            index(node[0], node[1])
+            for node in neighbours(current_node[0], current_node[1])
+        ]
+
+        for neighbour in neighs:
+            pass
 
 
 def main(argv: Sequence[str] | None = None) -> int:
@@ -117,7 +130,7 @@ def main(argv: Sequence[str] | None = None) -> int:
 
     minimal_heights = find_minimal_heights(heatmap)
 
-    print(sum(pos[1] for pos in minimal_heights))
+    print(sum(heatmap[index] + 1 for index in minimal_heights))
 
     return 0
 
