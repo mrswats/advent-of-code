0602b2773851eb390ae9d9c604daf662f09b5b01
Ferran Jovell <ferran@jovell.dev>
2022-12-01 19:31:09 +0100

    Day 9, part 2 DONE!

--- src/day_9/main.py
+++ src/day_9/main.py
@@ -1,5 +1,5 @@
 import argparse
-from dataclasses import dataclass
+from functools import reduce
 from typing import Sequence, Set
 
 WIDTH = 100
@@ -31,54 +31,54 @@ def index(row: int, col: int) -> int:
 def neighbours(row_idx: int, col_idx: int) -> Set[int]:
     if row_idx == TOP_EDGE and col_idx == LEFT_EDGE:
         return {
-            index(row_idx, col_idx + 1),
-            index(row_idx + 1, col_idx),
+            (row_idx, col_idx + 1),
+            (row_idx + 1, col_idx),
         }
     elif row_idx == TOP_EDGE and col_idx == RIGHT_EDGE:
         return {
-            index(row_idx, col_idx - 1),
-            index(row_idx + 1, col_idx),
+            (row_idx, col_idx - 1),
+            (row_idx + 1, col_idx),
         }
     elif row_idx == BOTTOM_EDGE and col_idx == LEFT_EDGE:
         return {
-            index(row_idx, col_idx + 1),
-            index(row_idx - 1, col_idx),
+            (row_idx, col_idx + 1),
+            (row_idx - 1, col_idx),
         }
     elif row_idx == BOTTOM_EDGE and col_idx == RIGHT_EDGE:
         return {
-            index(row_idx, col_idx - 1),
-            index(row_idx - 1, col_idx),
+            (row_idx, col_idx - 1),
+            (row_idx - 1, col_idx),
         }
     elif col_idx == LEFT_EDGE:
         return {
-            index(row_idx, col_idx + 1),
-            index(row_idx + 1, col_idx),
-            index(row_idx - 1, col_idx),
+            (row_idx, col_idx + 1),
+            (row_idx + 1, col_idx),
+            (row_idx - 1, col_idx),
         }
     elif col_idx == RIGHT_EDGE:
         return {
-            index(row_idx, col_idx - 1),
-            index(row_idx + 1, col_idx),
-            index(row_idx - 1, col_idx),
+            (row_idx, col_idx - 1),
+            (row_idx + 1, col_idx),
+            (row_idx - 1, col_idx),
         }
     elif row_idx == TOP_EDGE:
         return {
-            index(row_idx, col_idx - 1),
-            index(row_idx, col_idx + 1),
-            index(row_idx + 1, col_idx),
+            (row_idx, col_idx - 1),
+            (row_idx, col_idx + 1),
+            (row_idx + 1, col_idx),
         }
     elif row_idx == BOTTOM_EDGE:
         return {
-            index(row_idx, col_idx - 1),
-            index(row_idx, col_idx + 1),
-            index(row_idx - 1, col_idx),
+            (row_idx, col_idx - 1),
+            (row_idx, col_idx + 1),
+            (row_idx - 1, col_idx),
         }
     else:
         return {
-            index(row_idx, col_idx - 1),
-            index(row_idx, col_idx + 1),
-            index(row_idx - 1, col_idx),
-            index(row_idx + 1, col_idx),
+            (row_idx, col_idx - 1),
+            (row_idx, col_idx + 1),
+            (row_idx - 1, col_idx),
+            (row_idx + 1, col_idx),
         }
 
 
@@ -88,38 +88,49 @@ def is_minima(height: int, *height_adjacent_edges: int) -> bool:
 
 def find_minimal_heights(heatmap: list[int]) -> list[int]:
     return [
-        index(row_idx, col_idx)
+        (row_idx, col_idx)
         for row_idx in range(HEIGHT)
         for col_idx in range(WIDTH)
         if is_minima(
             heatmap[index(row_idx, col_idx)],
-            *(heatmap[idx] for idx in neighbours(row_idx, col_idx)),
+            *(heatmap[index(row, col)] for row, col in neighbours(row_idx, col_idx)),
         )
     ]
 
 
 def search(heatmap: list[int]):
     starting_nodes = find_minimal_heights(heatmap)
-    all_nodes = [
-        (row_idx, col_idx)
-        for row_idx in range(HEIGHT)
-        for col_idx in range(WIDTH)
-        if index(row_idx, col_idx) not in starting_nodes
-    ]
 
-    visited_nodes = []
+    biggest_basins_len = []
 
-    current_node = starting_nodes[0]
+    for starting_node in starting_nodes:
+        nodes_to_visit = [starting_node]
+        visited_nodes = []
 
-    while True:
-        visited_nodes.append(current_node)
-        neighs = [
-            index(node[0], node[1])
-            for node in neighbours(current_node[0], current_node[1])
-        ]
+        while True:
+            if not nodes_to_visit:
+                break
 
-        for neighbour in neighs:
-            pass
+            current_node = nodes_to_visit.pop(0)
+            visited_nodes.append(current_node)
+
+            for node in neighbours(current_node[0], current_node[1]):
+                if (
+                    node not in visited_nodes
+                    and node not in nodes_to_visit
+                    and heatmap[index(node[0], node[1])] < 9
+                ):
+                    nodes_to_visit.append(node)
+
+        current_min = min(biggest_basins_len) if biggest_basins_len else 0
+        basin_size = len(visited_nodes)
+        if current_min < basin_size:
+            if current_min in biggest_basins_len and len(biggest_basins_len) == 3:
+                biggest_basins_len.remove(current_min)
+
+            biggest_basins_len.append(basin_size)
+
+    return biggest_basins_len
 
 
 def main(argv: Sequence[str] | None = None) -> int:
@@ -128,9 +139,13 @@ def main(argv: Sequence[str] | None = None) -> int:
     args = parser.parse_args(argv)
     heatmap = parse_input(args.filename)
 
-    minimal_heights = find_minimal_heights(heatmap)
+    biggest_basins = search(heatmap)
 
-    print(sum(heatmap[index] + 1 for index in minimal_heights))
+    print(reduce(lambda a, b: a * b, biggest_basins))
+
+    # minimal_heights = find_minimal_heights(heatmap)
+
+    # print(sum(heatmap[index] + 1 for index in minimal_heights))
 
     return 0
 
