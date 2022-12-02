28864a4e81b4dfa037f418426fb3bc4eef03f45d
Ferran Jovell <ferran.jovell@hybridtheory.com>
2022-12-02 12:52:36 +0100

    Day 2: Part 2 DONE!

--- day_2/main.py
+++ day_2/main.py
@@ -2,27 +2,50 @@ import argparse
 from typing import Sequence
 
 """
-A, X: Rock -> 1
-B, Y: Paper -> 2
-C, Z: Scissors -> 3
+A: Rock -> 1
+B: Paper -> 2
+C: Scissors -> 3
+
+X: LOSE
+Y: DRAW
+Z: WIN
 """
 
+possible_choices = {
+    "A": "ROCK",
+    "B": "PAPER",
+    "C": "SCISSORS",
+}
+
+possible_outcomes = {
+    "X": "LOSE",
+    "Y": "DRAW",
+    "Z": "WIN",
+}
+
+outcome_table = {
+    "WIN": {
+        "A": "B",
+        "B": "C",
+        "C": "A",
+    },
+    "LOSE": {
+        "A": "C",
+        "B": "A",
+        "C": "B",
+    },
+}
+
 choice = {
-    "X": 1,
-    "Y": 2,
-    "Z": 3,
+    "A": 1,
+    "B": 2,
+    "C": 3,
 }
 
 points = {
-    ("A", "X"): 3,
-    ("A", "Y"): 6,
-    ("A", "Z"): 0,
-    ("B", "X"): 0,
-    ("B", "Y"): 3,
-    ("B", "Z"): 6,
-    ("C", "X"): 6,
-    ("C", "Y"): 0,
-    ("C", "Z"): 3,
+    "WIN": 6,
+    "DRAW": 3,
+    "LOSE": 0,
 }
 
 
@@ -31,13 +54,38 @@ def parse_input(filename: str) -> list[str]:
         return [tuple(line.strip().split(" ")) for line in f.readlines()]
 
 
+def select_choice(outcome: str, opponent: str) -> str:
+    if outcome == "DRAW":
+        return opponent
+    elif outcome == "WIN" or outcome == "LOSE":
+        return outcome_table[outcome][opponent]
+
+    raise AssertionError(f"Something went wrong, cant deal with outcome {outcome}")
+
+
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument("filename")
+    parser.add_argument("--debug", action=argparse.BooleanOptionalAction, default=False)
     args = parser.parse_args(argv)
     raw_input = parse_input(args.filename)
 
-    total_score = sum(points[round] + choice[round[1]] for round in raw_input)
+    total_score = 0
+    for round in raw_input:
+        opponent, expected_outcome = round
+        my_choice = select_choice(possible_outcomes[expected_outcome], opponent)
+        if args.debug:
+            print(
+                "Desired outcome: {}, opponent choice: {}, my choice: {} "
+                "score: {} + {}".format(
+                    possible_outcomes[expected_outcome],
+                    possible_choices[opponent],
+                    possible_choices[my_choice],
+                    choice[my_choice],
+                    points[possible_outcomes[expected_outcome]],
+                )
+            )
+        total_score += points[possible_outcomes[expected_outcome]] + choice[my_choice]
 
     print(total_score)
 
