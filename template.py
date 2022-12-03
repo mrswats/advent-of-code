3ce0d92249f2c16afbe36ff0dec42b32f12cc7e2
Ferran Jovell <ferran@jovell.dev>
2022-12-03 20:41:06 +0100

    Updated the template

--- template.py
+++ template.py
@@ -1,19 +1,26 @@
 import argparse
 from typing import Sequence
 
+INPUT = "input.txt"
+TEST_INPUT = """
+"""
 
-def parse_input(filename: str) -> list[str]:
+
+def solve(raw_input: str) -> str | int:
+    return 0
+
+
+def parse_input(filename: str) -> str:
     with open(filename) as f:
-        return f.readlines()
+        return f.read()
 
 
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument("filename")
+    parser.add_argument("--test", action=argparse.BooleanOptionalAction, default=True)
     args = parser.parse_args(argv)
-    raw_input = parse_input(args.filename)
-
-    print(raw_input)
+    raw_input = TEST_INPUT if args.test else parse_input(INPUT)
+    print(solve(raw_input))
 
     return 0
 
