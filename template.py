0ca56f3a9fb22328eebf19e0350497bd89c09c47
Ferran Jovell <ferran@jovell.dev>
2022-12-03 23:21:42 +0100

    Template, new-day script, gitignore, etc...

---
+++ template.py
@@ -0,0 +1,29 @@
+import argparse
+from typing import Sequence
+
+INPUT = "input.txt"
+TEST_INPUT = """
+"""
+
+
+def solve(raw_input: str) -> str | int:
+    return 0
+
+
+def parse_input(filename: str) -> str:
+    with open(filename) as f:
+        return f.read()
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test", action=argparse.BooleanOptionalAction, default=True)
+    args = parser.parse_args(argv)
+    raw_input = TEST_INPUT if args.test else parse_input(INPUT)
+    print(solve(raw_input))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
