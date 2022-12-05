4f9acf5845604a4d5b63d90416041f7592410078
Ferran Jovell <ferran@jovell.dev>
2022-12-05 17:45:13 +0100

    2021: Day 11: Part 1: WIP

--- 2021/day_11/main.py
+++ 2021/day_11/main.py
@@ -1,8 +1,114 @@
 import argparse
-from typing import Sequence
+from typing import Sequence, Set
 
 TOTAL_STEPS = 100
-SIZE = 10
+SIZE = 9
+TOP_EDGE = 0
+BOTTOM_EDGE = SIZE
+LEFT_EDGE = 0
+RIGHT_EDGE = SIZE
+
+
+def print_octopuses(octopuses) -> None:
+    for idx, energy in enumerate(octopuses):
+        print(energy, end=" ")
+        if (idx + 1) % SIZE == 0:
+            print()
+    print()
+
+
+def increase_energy(octopuses: list[int]) -> list[int]:
+    return [energy + 1 for energy in octopuses]
+
+
+def solve(octopuses: list[int]) -> int:
+    total_flashes = 0
+
+    for _ in range(TOTAL_STEPS):
+        octopuses = increase_energy(octopuses)
+        while any(octopus > 9 for octopus in octopuses):
+            for row_idx in range(SIZE):
+                for col_idx in range(SIZE):
+                    if octopuses[index(row_idx, col_idx)] > 9:
+                        for neighbour in neighbours(row_idx, col_idx):
+                            octopuses[index(*neighbour)] += 1
+
+        total_flashes += sum(octopus for octopus in octopuses if octopus > 9)
+        octopuses = [0 if octopus > 9 else octopus for octopus in octopuses]
+        if _ == 2:
+            break
+
+    return total_flashes
+
+
+def neighbours(row_idx: int, col_idx: int) -> Set[int]:
+    if row_idx == TOP_EDGE and col_idx == LEFT_EDGE:
+        return {
+            (row_idx, col_idx + 1),
+            (row_idx + 1, col_idx),
+            (row_idx + 1, col_idx + 1),
+        }
+    elif row_idx == TOP_EDGE and col_idx == RIGHT_EDGE:
+        return {
+            (row_idx, col_idx - 1),
+            (row_idx + 1, col_idx),
+            (row_idx + 1, col_idx - 1),
+        }
+    elif row_idx == BOTTOM_EDGE and col_idx == LEFT_EDGE:
+        return {
+            (row_idx, col_idx + 1),
+            (row_idx - 1, col_idx),
+            (row_idx - 1, col_idx + 1),
+        }
+    elif row_idx == BOTTOM_EDGE and col_idx == RIGHT_EDGE:
+        return {
+            (row_idx, col_idx - 1),
+            (row_idx - 1, col_idx),
+            (row_idx - 1, col_idx - 1),
+        }
+    elif col_idx == LEFT_EDGE:
+        return {
+            (row_idx, col_idx + 1),
+            (row_idx + 1, col_idx),
+            (row_idx - 1, col_idx),
+            (row_idx - 1, col_idx + 1),
+            (row_idx + 1, col_idx + 1),
+        }
+    elif col_idx == RIGHT_EDGE:
+        return {
+            (row_idx, col_idx - 1),
+            (row_idx + 1, col_idx),
+            (row_idx - 1, col_idx),
+            (row_idx + 1, col_idx - 1),
+            (row_idx - 1, col_idx - 1),
+        }
+    elif row_idx == TOP_EDGE:
+        return {
+            (row_idx, col_idx - 1),
+            (row_idx, col_idx + 1),
+            (row_idx + 1, col_idx),
+            (row_idx + 1, col_idx + 1),
+            (row_idx + 1, col_idx - 1),
+        }
+    elif row_idx == BOTTOM_EDGE:
+        return {
+            (row_idx, col_idx - 1),
+            (row_idx, col_idx + 1),
+            (row_idx - 1, col_idx),
+            (row_idx - 1, col_idx - 1),
+            (row_idx - 1, col_idx + 1),
+        }
+    else:
+        return {
+            (row_idx, col_idx - 1),
+            (row_idx, col_idx + 1),
+            (row_idx - 1, col_idx),
+            (row_idx + 1, col_idx),
+            (row_idx - 1, col_idx - 1),
+            (row_idx + 1, col_idx + 1),
+            (row_idx - 1, col_idx + 1),
+            (row_idx + 1, col_idx - 1),
+        }
 
 
 def index(row: int, col: int) -> int:
@@ -20,12 +126,7 @@ def main(argv: Sequence[str] | None = None) -> int:
     args = parser.parse_args(argv)
     octopuses = parse_input(args.filename)
 
-    total_flashes = 0
-
-    for _ in range(TOTAL_STEPS):
-        pass
-
-    print(total_flashes)
+    print(solve(octopuses))
 
     return 0
 
