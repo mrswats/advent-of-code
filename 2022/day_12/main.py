5b31177cd117b9461821e41ec9ab3a158d80da0f
Ferran Jovell <ferran@jovell.dev>
2022-12-17 17:54:48 +0100

    2022: Day 12: Part 1 DONE!

--- 2022/day_12/main.py
+++ 2022/day_12/main.py
@@ -3,12 +3,13 @@ from __future__ import annotations
 import argparse
 from dataclasses import dataclass
 from math import inf
-from pprint import pprint as print
-from typing import Sequence, Set
+from typing import Sequence, Tuple
+
+Point = Tuple[int, int]
 
 INPUT = "input.txt"
 TEST_INPUT = """\
-aabqponm
+Sabqponm
 abcryxxl
 accszExk
 acctuvwj
@@ -32,128 +33,85 @@ class Node:
     visited = False
     prev: Node | None = None
 
+    def __hash__(self) -> int:
+        return hash((self.pos_x, self.pos_y))
+
     def __repr__(self) -> str:
-        return (
-            f"Node({self.node_id=}, pos=({self.pos_x}, {self.pos_y}), {self.height=})"
-        )
+        prev = self.prev.node_id if self.prev else None
+        return f"Node({self.node_id}, pos=({self.pos_x}, {self.pos_y}), prev={prev}, dist={self.dist})"
 
 
-def neighbours(row_idx: int, col_idx: int) -> Set[int]:
-    if row_idx == TOP_EDGE and col_idx == LEFT_EDGE:
-        return {
-            (row_idx, col_idx + 1),
-            (row_idx + 1, col_idx),
-        }
-    elif row_idx == TOP_EDGE and col_idx == RIGHT_EDGE:
-        return {
-            (row_idx, col_idx - 1),
-            (row_idx + 1, col_idx),
-        }
-    elif row_idx == BOTTOM_EDGE and col_idx == LEFT_EDGE:
-        return {
-            (row_idx, col_idx + 1),
-            (row_idx - 1, col_idx),
-        }
-    elif row_idx == BOTTOM_EDGE and col_idx == RIGHT_EDGE:
-        return {
-            (row_idx, col_idx - 1),
-            (row_idx - 1, col_idx),
-        }
-    elif col_idx == LEFT_EDGE:
-        return {
-            (row_idx, col_idx + 1),
-            (row_idx + 1, col_idx),
-            (row_idx - 1, col_idx),
-        }
-    elif col_idx == RIGHT_EDGE:
-        return {
-            (row_idx, col_idx - 1),
-            (row_idx + 1, col_idx),
-            (row_idx - 1, col_idx),
-        }
-    elif row_idx == TOP_EDGE:
-        return {
-            (row_idx, col_idx - 1),
-            (row_idx, col_idx + 1),
-            (row_idx + 1, col_idx),
-        }
-    elif row_idx == BOTTOM_EDGE:
-        return {
-            (row_idx, col_idx - 1),
-            (row_idx, col_idx + 1),
-            (row_idx - 1, col_idx),
-        }
-    else:
-        return {
-            (row_idx, col_idx - 1),
-            (row_idx, col_idx + 1),
-            (row_idx - 1, col_idx),
-            (row_idx + 1, col_idx),
-        }
-
-
-def mdist(node_a: Node, node_b: Node) -> int:
-    return abs(node_a.pos_x - node_b.pos_x) + abs(node_a.pos_y - node_b.pos_y)
+def neighbours(row_idx: int, col_idx: int):
+    return (
+        (row_idx + 1, col_idx),
+        (row_idx - 1, col_idx),
+        (row_idx, col_idx + 1),
+        (row_idx, col_idx - 1),
+    )
 
 
 def find_destination(nodes: list[Node]) -> Node:
-    for node in nodes:
+    for node in nodes.values():
         if node.label == END_LABEL:
             return node
 
 
-def find_by_coordinates(nodes: list[Node], x_coord: int, y_coord: int) -> Node:
-    for node in nodes:
-        if not node.visited and node.pos_x == x_coord and node.pos_y == y_coord:
-            return node
-
-
-def solve(heightmap: str) -> str | int:
-    nodes = []
+def solve(heightmap: str) -> int:
+    nodes = {}
+    start = end = None
 
     def node_id(row: int, col: int) -> int:
         return row * len(heightmap.splitlines()) + col
 
     for rowidx, row in enumerate(heightmap.splitlines()):
         for colidx, col in enumerate(row):
-            nodes.append(
-                Node(
-                    node_id=node_id(rowidx, colidx),
-                    pos_x=colidx,
-                    pos_y=rowidx,
-                    height=ord(col),
-                    label=col,
-                )
+            node = Node(
+                node_id=node_id(rowidx, colidx),
+                pos_x=colidx,
+                pos_y=rowidx,
+                height=ord(col),
+                label=col,
             )
+            nodes[(colidx, rowidx)] = node
+            if col == "E":
+                end = node
+            elif col == "S":
+                start = node
 
-    end_node = find_destination(nodes)
-    start_node = nodes[0]
-    start_node.visited = True
-    assert start_node.node_id == 0, start_node.node_id
+    start.height = ord("a")
+    start.dist = 0
+    end.height = ord("z") + 1
 
-    neighbour_nodes = [start_node]
+    visited_nodes = set()
+    todo = [start]
 
-    while neighbour_nodes:
-        current_node = neighbour_nodes.pop(0)
+    while todo:
+        current_node = todo.pop(0)
 
-        print(current_node)
-
-        if current_node == end_node:
+        if current_node == end:
             break
+        if current_node in visited_nodes:
+            continue
+
+        visited_nodes.add(current_node)
 
         for neighbour in neighbours(current_node.pos_x, current_node.pos_y):
-            neighbour_node = find_by_coordinates(nodes, *neighbour)
-            if (
-                neighbour_node is not None
-                and not neighbour_node.visited
-                and 0 <= current_node.height - neighbour_node.height <= 1
-            ):
-                neighbour_node.visited = True
-                neighbour_node.dist = mdist(neighbour_node, end_node)
-                neighbour_node.prev = current_node
-                neighbour_nodes.append(neighbour_node)
+            if neighbour not in nodes:
+                continue
 
-    return nodes
+            neighbour_node = nodes[*neighbour]
+
+            if (
+                neighbour_node not in visited_nodes
+                and neighbour_node.height - current_node.height <= 1
+            ):
+                neighbour_node.prev = current_node
+                neighbour_node.dist = current_node.dist + 1
+                todo.append(neighbour_node)
+
+        todo.sort(key=lambda node: node.dist)
+
+    return end.dist
 
 
 def read_input_file(filename: str) -> str:
