6c1271a2751e89136ea981b15b4b5480270b2bd8
Ferran Jovell <ferran@jovell.dev>
2022-12-17 19:30:18 +0100

    2022: Day 12: Part 2: DONE!

--- 2022/day_12/main.py
+++ 2022/day_12/main.py
@@ -15,11 +15,10 @@ accszExk
 acctuvwj
 abdefghi
 """
-TOP_EDGE = 0
-LEFT_EDGE = 0
-BOTTOM_EDGE = 4
-RIGHT_EDGE = 7
+LOWEST_POINT = "a"
+
 END_LABEL = "E"
+START_LABEL = "S"
 
 
 @dataclass
@@ -33,12 +32,15 @@ class Node:
     visited = False
     prev: Node | None = None
 
+    def mdist(self, other: Node) -> int:
+        return abs(self.pos_x - other.pos_x) + abs(self.pos_y - other.pos_y)
+
     def __hash__(self) -> int:
         return hash((self.pos_x, self.pos_y))
 
     def __repr__(self) -> str:
         prev = self.prev.node_id if self.prev else None
-        return f"Node({self.node_id}, pos=({self.pos_x}, {self.pos_y}), prev={prev}, dist={self.dist})"
+        return f"Node({self.node_id}({self.label}), pos=({self.pos_x}, {self.pos_y}), prev={prev}, dist={self.dist})"
 
 
 def neighbours(row_idx: int, col_idx: int):
@@ -50,15 +52,9 @@ def neighbours(row_idx: int, col_idx: int):
     )
 
 
-def find_destination(nodes: list[Node]) -> Node:
-    for node in nodes.values():
-        if node.label == END_LABEL:
-            return node
-
-
 def solve(heightmap: str) -> int:
     nodes = {}
-    start = end = None
+    end = None
 
     def node_id(row: int, col: int) -> int:
         return row * len(heightmap.splitlines()) + col
@@ -73,45 +69,40 @@ def solve(heightmap: str) -> int:
                 label=col,
             )
             nodes[(colidx, rowidx)] = node
-            if col == "E":
+            if col == END_LABEL:
                 end = node
-            elif col == "S":
-                start = node
 
-    start.height = ord("a")
-    start.dist = 0
     end.height = ord("z") + 1
+    end.dist = 0
 
     visited_nodes = set()
-    todo = [start]
+    todo = [end]
 
     while todo:
         current_node = todo.pop(0)
 
-        if current_node == end:
-            break
-        if current_node in visited_nodes:
+        if current_node.label == "a":
+            return current_node.dist
+        elif current_node in visited_nodes:
             continue
-
-        visited_nodes.add(current_node)
+        else:
+            visited_nodes.add(current_node)
 
         for neighbour in neighbours(current_node.pos_x, current_node.pos_y):
             if neighbour not in nodes:
                 continue
 
-            neighbour_node = nodes[*neighbour]
+            neighbour_node = nodes[neighbour]
 
             if (
                 neighbour_node not in visited_nodes
-                and neighbour_node.height - current_node.height <= 1
+                and neighbour_node.height - current_node.height >= -1
             ):
                 neighbour_node.prev = current_node
                 neighbour_node.dist = current_node.dist + 1
                 todo.append(neighbour_node)
 
         todo.sort(key=lambda node: node.dist)
-
-    return end.dist
 
 
 def read_input_file(filename: str) -> str:
