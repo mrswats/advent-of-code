0040899d8ba4d45ec1152494c36ffdf6cff93583
Ferran Jovell <ferran@jovell.dev>
2023-01-01 22:31:57 +0100

    2022: Day 17: Part 1 done!

--- 2022/day_17/main.py
+++ 2022/day_17/main.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import argparse
-import collections
 import copy
 import itertools
 from typing import Any, Sequence, Tuple
@@ -22,32 +21,39 @@ moves = {
 }
 
 pieces = [
-    [(0, 0), (1, 0), (2, 0), (3, 0)],  # _
-    [(0, 0), (-1, 0), (1, 0), (0, 1), (0, -1)],  # +
-    [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)],  # reversed L
-    [(0, 0), (0, 1), (0, 2), (0, 3)],  # |+
-    [(0, 0), (1, 0), (0, 1), (1, 1)],  # cube
+    [(2, 0), (3, 0), (4, 0), (5, 0)],  # _
+    [(3, 1), (2, 1), (4, 1), (3, 2), (3, 0)],  # +
+    [(2, 0), (3, 0), (4, 0), (4, 1), (4, 2)],  # reversed L
+    [(2, 0), (2, 1), (2, 2), (2, 3)],  # |+
+    [(2, 0), (3, 0), (2, 1), (3, 1)],  # cube
 ]
 
 Point = Tuple[int, int]
 
 
 def highest_point_in_the_cavern(cavern: dict[Point, str]) -> int:
-    return max(point[1] for point in cavern.keys())
+    return max(point[1] for point in cavern.keys()) + 1
 
 
-def print_cavern(cavern: dict[Point, str]) -> None:
-    highest = highest_point_in_the_cavern(cavern) + 3
+def print_cavern(cavern: dict[Point, str], piece: list[Point] | None = None) -> None:
+    highest = highest_point_in_the_cavern(cavern) + 5
+    cavern = copy.copy(cavern)
+
+    if piece:
+        for piecepos in piece:
+            cavern[piecepos] = "@"
 
     print()
 
-    for row in reversed(range(highest)):
+    for row in reversed(range(-1, highest)):
         for column in range(-1, WIDTH + 1):
             if column in [-1, WIDTH]:
-                if row == 0:
+                if row == -1:
                     print("+", end="")
                 else:
                     print("|", end="")
+            elif (column, row) not in cavern:
+                print(".", end="")
             else:
                 print(cavern[(column, row)], end="")
         print()
@@ -56,16 +62,16 @@ def print_cavern(cavern: dict[Point, str]) -> None:
 
 
 def generate_cavern() -> dict[Point, str]:
-    cavern = collections.defaultdict(lambda: ".")
+    cavern = {}
 
     for col in range(WIDTH):
-        cavern[(col, 0)] = "-"
+        cavern[(col, -1)] = "-"
 
     return cavern
 
 
 def in_bounds(piece: list[Point]) -> bool:
-    return all(0 <= x < WIDTH for x, _ in piece)
+    return all(0 <= x < WIDTH and 0 <= y for x, y in piece)
 
 
 def push_piece(piece: list[Point], direction: Point) -> list[Point]:
@@ -78,38 +84,39 @@ def collision(piece, cavern) -> bool:
     return any(piecepos in cavern for piecepos in piece)
 
 
-def next_movement(jet_data: list[str]) -> Point:
-    for index, direction in enumerate(itertools.cycle(jet_data)):
-        if index % 2 != 0:
-            yield moves["v"]
-        else:
-            yield moves[direction]
-
-
-def solve_part1(parsed_data: str) -> str | int:
+def solve_part1(jet_data: str) -> str | int:
 
     pieces_fallen = 0
 
     cavern = generate_cavern()
-    movements = next_movement(parsed_data)
+    movements = itertools.cycle(jet_data)
 
     for piece in itertools.cycle(pieces):
         pieces_fallen += 1
 
         current_height = highest_point_in_the_cavern(cavern)
-        piece = push_piece(piece, (0, current_height + 2))
+        piece = push_piece(piece, (0, current_height + 3))
+        index = 0
 
         while True:
-            direction = next(movements)
-            piece = push_piece(piece, direction)
+            movement = next(movements) if index % 2 == 0 else "v"
+            direction = moves[movement]
+            next_piece_position = push_piece(piece, direction)
+            if not collision(next_piece_position, cavern) or not in_bounds(
+                next_piece_position
+            ):
+                piece = next_piece_position
 
-            if piece == push_piece(piece, moves["v"]) or collision(piece, cavern):
-                for piecepos in push_piece(piece, moves["^"]):
-                    cavern[piecepos] = "#"
+            index += 1
 
+            next_piece_position = push_piece(piece, moves["v"])
+            if movement in ["<", ">"] and (
+                next_piece_position == piece or collision(next_piece_position, cavern)
+            ):
                 break
 
-        # print_cavern(copy.copy(cavern))
+        for piecepos in piece:
+            cavern[piecepos] = "#"
 
         if pieces_fallen == TOTAL_PIECES_FALLEN:
             break
